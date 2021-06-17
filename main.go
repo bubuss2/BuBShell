81d7a71065e9ecf7348a2a4c673d0a2b36fe838e
bubus2 <bubus258@gmail.com>
2021-06-17 21:27:46 +0200

    Implemented colors for display

--- main.go
+++ main.go
@@ -11,14 +11,19 @@ func main() {
 	// input reader
 	reader := bufio.NewReader(os.Stdin)
 
+	// color variables for colored output
+	colorGreen := "\033[32m"
+	colorRed := "\033[31m"
+	colorDefault := "\033[0m"
+
 	for {
 		currentPath, err := os.Getwd()
 		if err != nil {
 			log.Println(err)
 		}
 
-		fmt.Print(currentPath)
-		fmt.Print(" > ")
+		fmt.Print(string(colorGreen), currentPath)
+		fmt.Print(string(colorRed), " > ", string(colorDefault))
 
 		input, err := reader.ReadString('\n')
 		if err != nil {
